88fd0562eeffcc8f21c6af0958c6c8a48662acf3
RocketGirl62 <80389474+RocketGirl62@users.noreply.github.com>
2021-04-06 15:59:38 +0200

    Bug fix

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function variabeln() {
 function verschwinden(load) {
     if (load === 1) {
          try {
-            document.getElementById(button1).classList.remove('nonvisible');
+            document.getElementById('button1').classList.remove('nonvisible');
         } catch (error) {
 
         }
-        document.getElementById(button1).classList.add('visible');
+        document.getElementById('button1').classList.add('visible');
         
     } else {
         
         try {
-            document.getElementById(button1).classList.remove('visible');
+            document.getElementById('button1').classList.remove('visible');
         } catch (error) {
 
         }
-        document.getElementById(button1).classList.add('nonvisible');
+        document.getElementById('button1').classList.add('nonvisible');
         
     }
 }
