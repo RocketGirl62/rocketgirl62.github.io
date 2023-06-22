3aae25c66a8b41219f89764ca95b8c1bae558f50
RocketGirl62 <80389474+RocketGirl62@users.noreply.github.com>
2023-06-22 23:35:27 +0200

    Add files via upload

---
+++ MolareMasseRechner/elements.js
@@ -0,0 +1,10 @@
+class elements {
+    
+    constructor (o, m, i, n){
+        this.ordnungszahl = o;
+        this.masse = m;
+        this.m2 =  Math.floor(m*100);
+        this.iupac = i;
+        this.name = n;
+    }
+}
